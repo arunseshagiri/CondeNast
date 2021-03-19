baba03cf48bea534ae11af6500fb9419aab673d6
Arun kumar S <akumars@falabella.cl>
2021-03-19 15:34:28 +0530

    Added raw reponse model and domain model

---
+++ NewsUpdates/app/src/main/java/com/arunkumar/newsupdates/models/RawNewsModel.kt
@@ -0,0 +1,16 @@
+package com.arunkumar.newsupdates.models
+
+data class RawNewsModel(val status: String?, val totalResults: Int?, val articles: List<Article>?)
+
+data class Article(
+    val source: Source?,
+    val author: String?,
+    val title: String?,
+    val description: String?,
+    val url: String?,
+    val urlToImage: String?,
+    val publishedAt: String?,
+    val content: String?
+)
+
+data class Source(val id: String?, val name: String?)
